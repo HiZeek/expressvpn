75914a8450b30c12dacfe794a44f64bcce9924f1
Isaac Idibia <idibiaisaac3@gmail.com>
2023-01-04 08:45:36 +0100

    fix

--- sanityexpress/sanity.cli.ts
+++ sanityexpress/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import {defineCliConfig} from '@sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'eka36j6x',
-    dataset: 'production'
+    dataset: 'development'
   }
 })
