42a4607ce30217cf7c9999fa9236d444ec53aaf8
Isaac Idibia <idibiaisaac3@gmail.com>
2023-01-04 06:54:28 +0100

    fix

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -46,7 +46,9 @@ const Modal = (props: any) => {
         props.onClose();
     }
 
-    const portalElement = props.show ? (
+    const portalElement = document.getElementById("overlays");
+
+    const modalContent = props.show ? (
         <div className="fixed top-0 left-0 w-full z-40 h-screen bg-back" onClick={props.onClose}>
             <div className="fixed overflow-auto z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-10 rounded-[32px]">
                 <div className="pb-10 cursor-pointer flex flex-col items-end" onClick={handleClose}>
@@ -58,7 +60,7 @@ const Modal = (props: any) => {
     ) : null;
 
     if(isBrowser) {
-        return ReactDOM.createPortal(portalElement, document.getElementById('overlays'))
+        return ReactDOM.createPortal(modalContent, portalElement)
     } else {
         return null
     }
