b91b625c503ce147e469cb19cc61c6ba8776d441
Isaac Idibia <idibiaisaac3@gmail.com>
2023-01-04 07:01:12 +0100

    fix

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -4,37 +4,6 @@ import ReactDOM from "react-dom";
 import { useEffect, useState } from "react";
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 
-// const Backdrop = (props: any) => {
-//   return <div className="fixed w-full z-40 h-screen bg-black" onClick={props.onClose} />;
-// };
-
-// const ModalOverlay = (props: any) => {
-//   return (
-//     <div className="fixed overflow-auto phone:h-screen z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-//       <div>{props.children}</div>
-//     </div>
-//   );
-// };
-
-// const portalElement = document.getElementById("overlays");
-
-// const Modal = (props: any) => {
-//   return (
-//     <Fragment>
-//       {ReactDOM.createPortal(
-//         <Backdrop onClose={props.onClose} />,
-//         portalElement
-//       )}
-//       {ReactDOM.createPortal(
-//         <ModalOverlay>{props.children}</ModalOverlay>,
-//         portalElement
-//       )}
-//     </Fragment>
-//   );
-// };
-
-// export default Modal;
-
 const Modal = (props: any) => {
     const [isBrowser, setIsBrowser] = useState(false);
     useEffect(() => {
@@ -46,7 +15,7 @@ const Modal = (props: any) => {
         props.onClose();
     }
 
-    const portalElement = document.getElementById("overlays")!;
+    const portalElement = document.getElementById("#overlays")!;
 
     const modalContent = props.show ? (
         <div className="fixed top-0 left-0 w-full z-40 h-screen bg-back" onClick={props.onClose}>
